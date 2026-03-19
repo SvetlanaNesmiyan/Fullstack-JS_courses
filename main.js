058633d396045fdb13f80bd49a1ec9526ec5bd6c
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2026-03-19 18:12:20 +0000

    .

--- main.js
+++ main.js
@@ -1,60 +1,32 @@
-console.log('#54. JavaScript homework file');
+console.log('#55. JavaScript homework file');
 
 /*
  * #1
  */
 
-import { writeFile } from 'fs/promises';
+function asyncOperationDemo(callback) {
+  console.log('Перший виклик');
 
-async function writeFileAsync(filename, content) {
-  try {
-    await writeFile(filename, content);
-    console.log('Файл успішно записано');
-  } catch (error) {
-    console.error('Помилка при записі файлу:', error);
-  }
+  process.nextTick(() => {
+    console.log('Виконано nextTick');
+    callback('nextTick');
+  });
+
+  setImmediate(() => {
+    console.log('Виконано setImmediate');
+    callback('setImmediate');
+  });
+
+  setTimeout(() => {
+    console.log('Виконано setTimeout');
+    callback('setTimeout');
+  }, 0);
+
+  console.log('Останній виклик');
 }
 
-/*
- * #2
- */
+asyncOperationDemo(operation => {
+  console.log(`Завершено виконання: ${operation}`);
+});
 
-import { readFile } from 'fs/promises';
-
-async function readFileAsync(filename) {
-  try {
-    const content = await readFile(filename, 'utf8');
-    console.log('Файл успішно прочитано:', content);
-    return content;
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      console.error('Файл не існує:', filename);
-    } else {
-      console.error('Помилка при читанні файлу:', error);
-    }
-    throw error;
-  }
-}
-
-/*
- * #3
- */
-
-import { unlink } from 'fs/promises';
-
-async function deleteFileAsync(filename) {
-  try {
-    await unlink(filename);
-    console.log('Файл успішно видалено');
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      console.error('Файл не існує:', filename);
-    } else {
-      console.error('Помилка при видаленні файлу:', error);
-    }
-    throw error;
-  }
-}
-
-
-export { writeFileAsync, readFileAsync, deleteFileAsync };
+export { asyncOperationDemo };
